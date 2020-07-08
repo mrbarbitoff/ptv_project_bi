ef6de78c58a580c5f365d59dcc433810c91b180a
Yury Barbitoff <barbitoff@bk.ru>
2020-07-08 14:30:25 +0300

    ClinVar model

--- LoFR/predict.R
+++ LoFR/predict.R
@@ -3,14 +3,21 @@ library(randomForest)
 
 args = commandArgs(trailingOnly=TRUE)
 
-load(args[2])
-
 ptv_tab = read.table(args[1], sep='\t', header=T)
 ptv_tab$rescue_conserved = ptv_tab$rescue_loeuf_min <= ptv_tab$min_LOEUF
 
-predictions = predict(my_forest_b, ptv_tab, type='vote')
-ptv_tab$predict_prob = predictions[, 2]
-ptv_tab$LoF_confidence = ifelse(ptv_tab$predict_prob > 0.5, 'LOW',
-			   ifelse(ptv_tab$predict_prob > 0.25, 'MEDIUM', 'HIGH'))
+load(args[2])
+load(args[3])
+
+predictions_A = predict(my_forest_c, ptv_tab, type='vote')
+predictions_B = predict(my_forest_d, ptv_tab, type='vote')
+
+ptv_tab$Model_A_prob = predictions_A[, 2]
+ptv_tab$A_LoF_confidence = ifelse(ptv_tab$Model_A_prob > 0.5, 'LOW',
+			   ifelse(ptv_tab$Model_A_prob > 0.25, 'MEDIUM', 'HIGH'))
+
+ptv_tab$Model_B_prob = predictions_A[, 2]
+ptv_tab$B_LoF_confidence = ifelse(ptv_tab$Model_B_prob > 0.5, 'LOW',
+                           ifelse(ptv_tab$Model_B_prob > 0.25, 'MEDIUM', 'HIGH'))
 
 write.table(ptv_tab, file='results.tsv', sep='\t', row.names=F, quote=F)
