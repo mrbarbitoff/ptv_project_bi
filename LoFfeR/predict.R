1f74dcfdb9c5a600d60a91c8bb04678ee8ff7381
Yury Barbitoff <barbitoff@bk.ru>
2020-08-17 19:30:02 +0300

    Tool thresholds update

--- LoFfeR/predict.R
+++ LoFfeR/predict.R
@@ -12,12 +12,12 @@ load(args[3])
 predictions_A = predict(my_forest_c, ptv_tab, type='vote')
 predictions_B = predict(my_forest_d, ptv_tab, type='vote')
 
-ptv_tab$Model_A_prob = predictions_A[, 2]
-ptv_tab$A_LoF_confidence = ifelse(ptv_tab$Model_A_prob > 0.5, 'LOW',
-			   ifelse(ptv_tab$Model_A_prob > 0.25, 'MEDIUM', 'HIGH'))
+ptv_tab$Model_GN_A_prob = predictions_A[, 2]
+ptv_tab$GN_A_LoF_confidence = ifelse(ptv_tab$Model_GN_A_prob > 0.575, 'LOW',  
+				     ifelse(ptv_tab$Model_GN_A_prob > 0.25, 'MEDIUM', 'HIGH'))
 
-ptv_tab$Model_B_prob = predictions_B[, 2]
-ptv_tab$B_LoF_confidence = ifelse(ptv_tab$Model_B_prob > 0.5, 'LOW',
-                           ifelse(ptv_tab$Model_B_prob > 0.25, 'MEDIUM', 'HIGH'))
+ptv_tab$Model_CLV_prob = predictions_B[, 2]
+ptv_tab$CLV_LoF_confidence = ifelse(ptv_tab$Model_CLV_prob > 0.59, 'LOW',
+                           ifelse(ptv_tab$Model_CLV_prob > 0.25, 'MEDIUM', 'HIGH'))
 
 write.table(ptv_tab, file='results.tsv', sep='\t', row.names=F, quote=F)
